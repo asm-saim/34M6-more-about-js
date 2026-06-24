f3312e46255ea7e5f76ddf882c0884e1d3b7932f
asm-saim <saim777m@gmail.com>
2026-06-24 13:47:32 +0600

    call stsck, and event queue implemented

---
+++ 5.callStack-eventQueue.js
@@ -0,0 +1,20 @@
+//call stack -LIFO
+function one() {
+    two();
+    console.log(1);
+}
+function two() {
+    setTimeout(() => {
+        console.log("Even though I set in No:2 function, I moved to queue and called after the call stack")
+    }, 0)
+    three()
+    console.log(2);
+}
+function three() {
+    four();
+    console.log(3);
+}
+function four() {
+    console.log(4);
+}
+one();
